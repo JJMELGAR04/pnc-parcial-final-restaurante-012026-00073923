6b8e7f89488e65b66baf560ac48db6b3e84d97cd
Luisa Arévalo <larevalo173@gmail.com>
2026-07-07 19:53:27 -0600

    new: Adding indications

---
+++ src/main/java/com/uca/pncparcialfinalrestaurante/PncParcialFinalRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.uca.pncparcialfinalrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncParcialFinalRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PncParcialFinalRestauranteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uca/pncparcialfinalrestaurante/PncParcialFinalRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.pncparcialfinalrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncParcialFinalRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
